1d0e688934fb0b9db44bcc2da916ee98f47b38ee
Francisco Campos <franciiscocampos170@gmail.com>
2020-08-05 21:34:28 +0100

    feat: jwt instalado

--- app/User.php
+++ app/User.php
@@ -55,4 +55,25 @@ class User extends Authenticatable
      }
 
 
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+
+     public function getJWTCustomClaims(){
+         return [];
+     }
+
+
 }
